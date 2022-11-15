8c2e6c3556efffbaba1dd5b4d75236044d4e8bbc
Pedro <pedrodam99@gmail.com>
2022-11-14 21:47:05 -0300

    Funcionalidade de editar dados

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -19,7 +19,13 @@ module.exports = class ToughtsController {
 
     const toughts = user.Toughts.map((result) => result.dataValues);
 
-    res.render("toughts/dashboard", { toughts });
+    let emptyToughts = false;
+
+    if (toughts.length === 0) {
+      emptyToughts = true;
+    }
+
+    res.render("toughts/dashboard", { toughts, emptyToughts });
   }
 
   static createTought(req, res) {
@@ -36,6 +42,51 @@ module.exports = class ToughtsController {
       await Tought.create(tought);
 
       req.flash("message", "Pensamento criado com sucesso!");
+
+      req.session.save(() => {
+        res.redirect("/toughts/dashboard");
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  static async removeTought(req, res) {
+    const id = req.body.id;
+    const UserId = req.session.userid;
+
+    try {
+      await Tought.destroy({ where: { id: id, UserId: UserId } });
+
+      req.flash("message", "Pensamento removido com sucesso!");
+
+      req.session.save(() => {
+        res.redirect("/toughts/dashboard");
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  static async updateTought(req, res) {
+    const id = req.params.id;
+
+    const tought = await Tought.findOne({ where: { id: id }, raw: true });
+
+    res.render("toughts/edit", { tought });
+  }
+
+  static async updateToughtSave(req, res) {
+    const id = req.body.id;
+
+    const tought = {
+      title: req.body.title,
+    };
+
+    try {
+      await Tought.update(tought, { where: { id: id } });
+
+      req.flash("message", "Pensamento atualizado com sucesso!");
 
       req.session.save(() => {
         res.redirect("/toughts/dashboard");
